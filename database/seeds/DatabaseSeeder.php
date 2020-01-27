238321653ad60f462bc98ed967c050b0a4484201
nailainggar <nailainggar25@gmail.com>
2020-01-27 03:58:59 -0500

    membuat factory dan tinker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostsTableSeeder::class);
-        $this->call(PostsTableSiswa::class);
-        $this->call(PostsTablebio::class);
+        // $this->call(PostsTableSeeder::class);
+        // $this->call(PostsTableSiswa::class);
+        // $this->call(PostsTablebio::class);
+        factory(App\Tabungan::class, 100)->create();
+        factory(App\Customer::class, 1000)->create();
         // $this->call(UsersTableSeeder::class);
     }
 }
